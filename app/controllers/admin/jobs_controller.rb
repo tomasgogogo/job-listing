004683e014bfb0fbf18a550be8d79090c7de70a8
tomas <tomastong0108@gmail.com>
2017-01-07 19:13:50 +0800

    在admin::jobs controller里加入require_is_admin函数

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < ApplicationController
-
+  before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_filter :require_is_admin
 
   def show
     @job = Job.find(params[:id])
@@ -45,5 +46,12 @@ class Admin::JobsController < ApplicationController
 
   def job_params
     params.require(:job).permit(:title,:description)
+  end
+
+  def require_is_admin
+   if current_user.email != '1@1'
+     flash[:alert] = 'You are not admin'
+     redirect_to root_path
+   end
   end
 end
